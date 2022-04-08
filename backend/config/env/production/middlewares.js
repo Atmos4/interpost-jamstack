b463758f45b71b9f70679b066a38967f5652566a
Arnaud <arnaud.p@bokio.se>
2022-04-08 23:51:04 +0200

    Add more prod config

---
+++ backend/config/env/production/middlewares.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+];
--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,3 +1,6 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
+  app: {
+    keys: [env("APP_KEY_1"), env("APP_KEY_1")],
+  },
 });
